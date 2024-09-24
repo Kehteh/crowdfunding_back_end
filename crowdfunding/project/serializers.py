b9515f0ed1b685cefb3b598df6b4a584af9805c8
Katie <katie.connell.91@gmail.com>
2024-09-24 20:31:49 +0800

    Created pledge

--- crowdfunding/project/serializers.py
+++ crowdfunding/project/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from django.apps import apps
 
+class PledgeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = apps.get_model('project.Pledge')
+        fields = '__all__'
+
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = apps.get_model('project.Project')
         fields = '__all__'
+
+class ProjectDetailSerializer(ProjectSerializer):
+    pledges = PledgeSerializer(many=True, read_only=True)
