2c14a87ece9fbef8bde14a8668e27a27a61afbc0
Katie <katie.connell.91@gmail.com>
2024-12-15 17:43:20 +0800

    consolidated views

--- crowdfunding/project/urls.py
+++ crowdfunding/project/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('project/', views.ProjectList.as_view()),
     path('project/<int:pk>/', views.ProjectDetail.as_view()),
-    path('pledges/', views.PledgeList.as_view())
+    path('pledges/', views.PledgeList.as_view()),
+    path('pledges/<int:pk>/', views.PledgeDetail.as_view()),
+    # path('project/<int:pk>/', views.DeleteProjectView.as_view(), name='delete_project'),
 ]
